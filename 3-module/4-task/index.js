c33e1b113cd70ea74dcdf74b464b8a81d89683be
Lapuch <7070670@gmail.com>
2021-03-15 17:15:06 +0100

    HW 3module

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,12 @@
 function showSalary(users, age) {
-  // ваш код...
+  
+  let a =users.filter((user) => (user.age <= age));
+  let b= a.map((user, index)=> {
+    if (index!== a.length-1) {
+    return (`${user.name}, ${user.balance}\n`);
+    }else{
+      return (`${user.name}, ${user.balance}`);
+    }})
+  .join("");
+  return b;
 }
